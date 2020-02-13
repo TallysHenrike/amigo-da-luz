01ef3530ffeca256155964b3623f53e471a3ba76
Tallys Henrike <tallys.henrike@gmail.com>
2020-02-13 00:16:52 -0300

    Componente criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
-	{ path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+	{ path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule) },
+	{ path: 'doacao', loadChildren: () => import('./doacao/doacao.module').then( m => m.DoacaoPageModule) },
   {
     path: 'amigo-da-luz',
     loadChildren: () => import('./amigo-da-luz/amigo-da-luz.module').then( m => m.AmigoDaLuzPageModule)
